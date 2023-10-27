6e9d3e3d1ee7dd8315716da29fc56ce9cc814e5f
Long Nguyen <45144876+longnh299@users.noreply.github.com>
2023-10-27 15:27:15 +0700

    update Settings class to static

--- Assets/Scripts/Utilities/Settings.cs
+++ Assets/Scripts/Utilities/Settings.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Settings
+public static class Settings
 {
     public static int maxChildCorridors = 2; // max child corridor 
 }
