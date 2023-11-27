ef986a2e1ccee5ad5b643a553377a3b6308bb477
Long Nguyen <longkakapro2000@gmail.com>
2023-11-27 22:24:19 +0700

    Cinemachine target group

--- Assets/Scripts/Misc/CinemachineTarget.cs
+++ Assets/Scripts/Misc/CinemachineTarget.cs
@@ -8,6 +8,11 @@ public class CinemachineTarget : MonoBehaviour
 {
     private CinemachineTargetGroup cinemachineTargetGroup;
 
+    #region Tooltip
+    [Tooltip("Populate with the CursorTarget gameobject")]
+    #endregion Tooltip
+    [SerializeField] private Transform cursorTarget;
+
     private void Awake()
     {
         // load components
@@ -23,13 +28,20 @@ public class CinemachineTarget : MonoBehaviour
     // set the cinemachine camera target group.
     private void SetCinemachineTargetGroup()
     {
-        // create target group for cinemachine for the cinemachine camera to follow  - group will include the player and screen cursor
+        // Create target group for cinemachine for the cinemachine camera to follow  - group will include the player and screen cursor
         CinemachineTargetGroup.Target cinemachineGroupTarget_player = new CinemachineTargetGroup.Target { weight = 1f, radius = 2.5f, target = GameManager.Instance.GetPlayer().transform };
 
-        CinemachineTargetGroup.Target[] cinemachineTargetArray = new CinemachineTargetGroup.Target[] { cinemachineGroupTarget_player};
+        CinemachineTargetGroup.Target cinemachineGroupTarget_cursor = new CinemachineTargetGroup.Target { weight = 1f, radius = 1f, target = cursorTarget };
 
-        cinemachineTargetGroup.m_Targets = cinemachineTargetArray; // save cinemachine target into Targrt list of CinemachineTargetGroup in unity editor
+        CinemachineTargetGroup.Target[] cinemachineTargetArray = new CinemachineTargetGroup.Target[] { cinemachineGroupTarget_player, cinemachineGroupTarget_cursor };
 
+        cinemachineTargetGroup.m_Targets = cinemachineTargetArray;
+
+    }
+
+    private void Update()
+    {
+        cursorTarget.position = HelperUtilities.GetMouseWorldPosition();
     }
 
 }
