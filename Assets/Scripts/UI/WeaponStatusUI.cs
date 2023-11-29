b5634c351e7cceccd43b6abf33666a58ac7391e7
Long Nguyen <longkakapro2000@gmail.com>
2023-11-29 22:47:37 +0700

    create WeaponStatus UI

---
+++ Assets/Scripts/UI/WeaponStatusUI.cs
@@ -0,0 +1,328 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class WeaponStatusUI : MonoBehaviour
+{
+    #region Header OBJECT REFERENCES
+    [Space(10)]
+    [Header("OBJECT REFERENCES")]
+    #endregion Header OBJECT REFERENCES
+    #region Tooltip
+    [Tooltip("Populate with image component on the child WeaponImage gameobject")]
+    #endregion Tooltip
+    [SerializeField] private Image weaponImage;
+
+    #region Tooltip
+    [Tooltip("Populate with the Transform from the child AmmoHolder gameobject")]
+    #endregion Tooltip
+    [SerializeField] private Transform ammoHolderTransform;
+
+    #region Tooltip
+    [Tooltip("Populate with the TextMeshPro-Text component on the child ReloadText gameobject")]
+    #endregion Tooltip
+    [SerializeField] private TextMeshProUGUI reloadText;
+
+    #region Tooltip
+    [Tooltip("Populate with the TextMeshPro-Text component on the child AmmoRemainingText gameobject")]
+    #endregion Tooltip
+    [SerializeField] private TextMeshProUGUI ammoRemainingText;
+    #region Tooltip
+    [Tooltip("Populate with the TextMeshPro-Text component on the child WeaponNameText gameobject")]
+    #endregion Tooltip
+    [SerializeField] private TextMeshProUGUI weaponNameText;
+    #region Tooltip
+    [Tooltip("Populate with the RectTransform of the child gameobject ReloadBar")]
+    #endregion Tooltip
+    [SerializeField] private Transform reloadBar;
+    #region Tooltip
+    [Tooltip("Populate with the Image component of the child gameobject BarImage")]
+    #endregion Tooltip
+    [SerializeField] private Image barImage;
+
+    private Player player;
+    private List<GameObject> ammoIconList = new List<GameObject>();
+    private Coroutine reloadWeaponCoroutine;
+    private Coroutine blinkingReloadTextCoroutine;
+
+    private void Awake()
+    {
+        // Get player
+        player = GameManager.Instance.GetPlayer();
+    }
+
+    private void OnEnable()
+    {
+        // Subscribe to set active weapon event
+        player.setActiveWeaponEvent.OnSetActiveWeapon += SetActiveWeaponEvent_OnSetActiveWeapon;
+
+        // Subscribe to weapon fired event
+        player.weaponFiredEvent.OnWeaponFired += WeaponFiredEvent_OnWeaponFired;
+
+        // Subscribe to reload weapon event
+        player.reloadWeaponEvent.OnReloadWeapon += ReloadWeaponEvent_OnWeaponReload;
+
+        // Subscribe to weapon reloaded event
+        player.weaponReloadedEvent.OnWeaponReloaded += WeaponReloadedEvent_OnWeaponReloaded;
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribe from set active weapon event
+        player.setActiveWeaponEvent.OnSetActiveWeapon -= SetActiveWeaponEvent_OnSetActiveWeapon;
+
+        // Unsubscribe from weapon fired event
+        player.weaponFiredEvent.OnWeaponFired -= WeaponFiredEvent_OnWeaponFired;
+
+        // Unsubscribe from reload weapon event
+        player.reloadWeaponEvent.OnReloadWeapon -= ReloadWeaponEvent_OnWeaponReload;
+
+        // Unsubscribe from weapon reloaded event
+        player.weaponReloadedEvent.OnWeaponReloaded -= WeaponReloadedEvent_OnWeaponReloaded;
+    }
+
+    private void Start()
+    {
+        // Update active weapon status on the UI
+        SetActiveWeapon(player.activeWeapon.GetCurrentWeapon());
+    }
+
+    // Handle set active weapon event on the UI
+    private void SetActiveWeaponEvent_OnSetActiveWeapon(SetActiveWeaponEvent setActiveWeaponEvent, SetActiveWeaponEventArgs setActiveWeaponEventArgs)
+    {
+        SetActiveWeapon(setActiveWeaponEventArgs.weapon);
+    }
+
+    // Handle Weapon fired event on the UI
+    private void WeaponFiredEvent_OnWeaponFired(WeaponFiredEvent weaponFiredEvent, WeaponFiredEventArgs weaponFiredEventArgs)
+    {
+        WeaponFired(weaponFiredEventArgs.weapon);
+    }
+
+    // Weapon fired update UI
+    private void WeaponFired(Weapon weapon)
+    {
+        UpdateAmmoText(weapon);
+        UpdateAmmoLoadedIcons(weapon);
+        UpdateReloadText(weapon);
+    }
+
+    // Handle weapon reload event on the UI
+    private void ReloadWeaponEvent_OnWeaponReload(ReloadWeaponEvent reloadWeaponEvent, ReloadWeaponEventArgs reloadWeaponEventArgs)
+    {
+        UpdateWeaponReloadBar(reloadWeaponEventArgs.weapon);
+    }
+
+    // Handle weapon reloaded event on the UI
+    private void WeaponReloadedEvent_OnWeaponReloaded(WeaponReloadedEvent weaponReloadedEvent, WeaponReloadedEventArgs weaponReloadedEventArgs)
+    {
+        WeaponReloaded(weaponReloadedEventArgs.weapon);
+    }
+
+    // Weapon has been reloaded - update UI if current weapon
+    private void WeaponReloaded(Weapon weapon)
+    {
+        // if weapon reloaded is the current weapon
+        if (player.activeWeapon.GetCurrentWeapon() == weapon)
+        {
+            UpdateReloadText(weapon);
+            UpdateAmmoText(weapon);
+            UpdateAmmoLoadedIcons(weapon);
+            ResetWeaponReloadBar();
+        }
+    }
+
+    // Set the active weapon on the UI
+    private void SetActiveWeapon(Weapon weapon)
+    {
+        UpdateActiveWeaponImage(weapon.weaponDetails);
+        UpdateActiveWeaponName(weapon);
+        UpdateAmmoText(weapon);
+        UpdateAmmoLoadedIcons(weapon);
+
+        // If set weapon is still reloading then update reload bar
+        if (weapon.isWeaponReloading)
+        {
+            UpdateWeaponReloadBar(weapon);
+        }
+        else
+        {
+            ResetWeaponReloadBar();
+        }
+
+        UpdateReloadText(weapon);
+    }
+
+    // Populate active weapon image
+    private void UpdateActiveWeaponImage(WeaponDetailsSO weaponDetails)
+    {
+        weaponImage.sprite = weaponDetails.weaponSprite;
+    }
+
+    // Populate active weapon name
+    private void UpdateActiveWeaponName(Weapon weapon)
+    {
+        weaponNameText.text = "(" + weapon.weaponListPosition + ") " + weapon.weaponDetails.weaponName.ToUpper();
+    }
+
+    // Update the ammo remaining text on the UI
+    private void UpdateAmmoText(Weapon weapon)
+    {
+        if (weapon.weaponDetails.hasInfiniteAmmo)
+        {
+            ammoRemainingText.text = "INFINITE AMMO";
+        }
+        else
+        {
+            ammoRemainingText.text = weapon.weaponRemainingAmmo.ToString() + " / " + weapon.weaponDetails.weaponAmmoCapacity.ToString();
+        }
+    }
+
+    /// Update ammo clip icons on the UI
+    private void UpdateAmmoLoadedIcons(Weapon weapon)
+    {
+        ClearAmmoLoadedIcons();
+
+        for (int i = 0; i < weapon.weaponClipRemainingAmmo; i++)
+        {
+            // Instantiate ammo icon prefab
+            GameObject ammoIcon = Instantiate(GameResources.Instance.ammoIconPrefab, ammoHolderTransform);
+
+            ammoIcon.GetComponent<RectTransform>().anchoredPosition = new Vector2(0f, Settings.uiAmmoIconSpacing * i);
+
+            ammoIconList.Add(ammoIcon);
+        }
+    }
+
+    // Clear ammo icons
+    private void ClearAmmoLoadedIcons()
+    {
+        // Loop through icon gameobjects and destroy
+        foreach (GameObject ammoIcon in ammoIconList)
+        {
+            Destroy(ammoIcon);
+        }
+
+        ammoIconList.Clear();
+    }
+
+    // Reload weapon - update the reload bar on the UI
+    private void UpdateWeaponReloadBar(Weapon weapon)
+    {
+        if (weapon.weaponDetails.hasInfiniteClipCapacity)
+            return;
+
+        StopReloadWeaponCoroutine();
+        UpdateReloadText(weapon);
+
+        reloadWeaponCoroutine = StartCoroutine(UpdateWeaponReloadBarRoutine(weapon));
+    }
+
+    // Animate reload weapon bar coroutine
+    private IEnumerator UpdateWeaponReloadBarRoutine(Weapon currentWeapon)
+    {
+        // set the reload bar to red
+        barImage.color = Color.red;
+
+        // Animate the weapon reload bar
+        while (currentWeapon.isWeaponReloading)
+        {
+            // update reloadbar
+            float barFill = currentWeapon.weaponReloadTimer / currentWeapon.weaponDetails.weaponReloadTime;
+
+            // update bar fill
+            reloadBar.transform.localScale = new Vector3(barFill, 1f, 1f);
+
+            yield return null;
+        }
+    }
+
+    // Initialise the weapon reload bar on the UI
+    private void ResetWeaponReloadBar()
+    {
+        StopReloadWeaponCoroutine();
+
+        // set bar color as green
+        barImage.color = Color.green;
+
+        // set bar scale to 1
+        reloadBar.transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+
+    // Stop coroutine updating weapon reload progress bar
+    private void StopReloadWeaponCoroutine()
+    {
+        // Stop any active weapon reload bar on the UI
+        if (reloadWeaponCoroutine != null)
+        {
+            StopCoroutine(reloadWeaponCoroutine);
+        }
+    }
+
+    // Update the blinking weapon reload text
+    private void UpdateReloadText(Weapon weapon)
+    {
+        if ((!weapon.weaponDetails.hasInfiniteClipCapacity) && (weapon.weaponClipRemainingAmmo <= 0 || weapon.isWeaponReloading))
+        {
+            // set the reload bar to red
+            barImage.color = Color.red;
+
+            StopBlinkingReloadTextCoroutine();
+
+            blinkingReloadTextCoroutine = StartCoroutine(StartBlinkingReloadTextRoutine());
+        }
+        else
+        {
+            StopBlinkingReloadText();
+        }
+    }
+
+    // Start the coroutine to blink the reload weapon text
+    private IEnumerator StartBlinkingReloadTextRoutine()
+    {
+        while (true)
+        {
+            reloadText.text = "RELOAD";
+            yield return new WaitForSeconds(0.3f);
+            reloadText.text = "";
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+    // Stop the blinking reload text 
+    private void StopBlinkingReloadText()
+    {
+        StopBlinkingReloadTextCoroutine();
+
+        reloadText.text = "";
+    }
+
+    // Stop the blinking reload text coroutine
+    private void StopBlinkingReloadTextCoroutine()
+    {
+        if (blinkingReloadTextCoroutine != null)
+        {
+            StopCoroutine(blinkingReloadTextCoroutine);
+        }
+    }
+
+    #region Validation
+#if UNITY_EDITOR
+
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckNullValue(this, nameof(weaponImage), weaponImage);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(ammoHolderTransform), ammoHolderTransform);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(reloadText), reloadText);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(ammoRemainingText), ammoRemainingText);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(weaponNameText), weaponNameText);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(reloadBar), reloadBar);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(barImage), barImage);
+    }
+
+#endif
+    #endregion Validation
+
+}
