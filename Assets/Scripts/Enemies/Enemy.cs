1d22f945d6fd6a5ff2213358dc267a698aa7bd8f
Long Nguyen <longkakapro2000@gmail.com>
2023-11-30 23:12:09 +0700

    create enemies and bosses animation

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -19,11 +19,13 @@ public class Enemy : MonoBehaviour
     private CircleCollider2D circleCollider2D;
     private PolygonCollider2D polygonCollider2D;
     [HideInInspector] public SpriteRenderer[] spriteRendererArray;
+    [HideInInspector] public Animator animator;
 
     private void Awake()
     {
         circleCollider2D = GetComponent<CircleCollider2D>();
         polygonCollider2D = GetComponent<PolygonCollider2D>();
         spriteRendererArray = GetComponentsInChildren<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 }
